c2e64b24c4a87e529555f9d636019d6346191698
Sabdhx <syedaliabdullahshah5@gmail.com>
2024-02-13 10:57:32 +0500

    added maping programm

---
+++ index.js
@@ -0,0 +1,64 @@
+  // function generateQuote(){
+    //   const arrayofquotes = ["lun pay chard umer", "hambani meray lorday pay", "wo bhi sonay ka hay", "girain mujhay"];
+    //   let randomIndex = Math.round(Math.random() * arrayofquotes.length-1);
+    //   document.getElementById("paragraph").innerText = arrayofquotes[randomIndex];
+    // }
+
+// let secondElapsed =0;
+// let interval =null;
+    
+
+// function startclock(){
+
+// }
+
+// function stopclock(){
+  
+// }
+
+// function resetclock(){
+  
+// }
+// const array=(word)=>word.slice(word.lastIndexOf('.'))
+// console.log(array('index.3'))
+
+
+// const array=[1,2,3,4,5]
+// let newarray= array.map(i=>i)
+// console.log(newarray);
+
+// let array=(newarray)=>{
+//    return newarray.map(i=>i*3)
+// }
+//  console.log(array([1,2,3,4,5,6,6]))
+
+// let array = [1, 2, 3, 4, 5];
+
+// let result = array
+//     .map(x => x * 2) // Doubles each element
+//     .map(x => x.toString()) // Converts each element to a string
+//     .map(x => x.split('').reverse().join('')); // Reverses each string
+
+// console.log(result); // Output: [ '2', '4', '6', '8', '10' ] reversed
+
+// let array = [
+//   { name: 'John', age: 25 },
+//   { name: 'Alice', age: 30 },
+//   { name: 'Bob', age: 35 }
+// ];
+
+// let result = array
+//   .map(person => ({ name: person.name.toUpperCase(), age: person.age * 2 }));
+
+// console.log(result);
+// Output: [ { name: 'JOHN', age: 50 }, { name: 'ALICE', age: 60 }, { name: 'BOB', age: 70 } ]
+
+
+// const array=[{name:"ali",age:21},
+//              {name:"abdullah",age:23},
+//              {name:"shah",age:24},
+//              {name:"rizvi",age:25} ];
+
+//           let newarray=array.map(i=>({name:i.name.split('').reverse().join(''),age:i.age*2}))
+//           console.log(newarray)
+
